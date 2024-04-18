d42f9ce32b3b47b59062834150542db8a6a38c9a
Zach_Mason <zmason04@gmail.com>
2024-04-18 18:36:02 -0400

    includes first attempt at pypoll up through candidate list

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -4,27 +4,42 @@ import csv
 #create path
 csvpath = os.path.join('PyPoll','Resources','election_data.csv')
 
-candidates = []
+#define any and all variables
 row_count = 0
+winner_votes = 0
+#cand_name = ''
+candidate_list = []
 
-with open(csvpath, 'r') as csvfile:
+#open, read the csv file and then skip the header
+with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-
-# print(csvreader)
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
-    #total number of votes
+    #calculate the total number of votes by rows
     for row in csvreader:
         row_count +=1
     print(f"Total votes: {row_count}")
 
-#with open(csvpath, 'r') as csvfile:
-   # header = next(csvfile)
-    #cand_list = [candidate[2] for candidate in csvreader]
-    #cand_info = [[candidate,cand_list.count(candidate)] 
-     #                  for candidate in set(cand_list)]
-    #print()
+        #module 3.3 lists/dictionaires
+      
+        #if/else statement to tally votes and add cand names
+        #if not already there
+    
+    #hannah and dan help   
+    #if cand_name in candidate_list:
+    
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
 
-   # for row in csvreader:
+    for row in csvreader:
+        cand_name = row[2]
         
+        if cand_name not in candidate_list:
+            candidate_list.append(cand_name)
+        
+    print(f"Candidates List: ")
+
+    for cand_name in candidate_list:
+        print(f"{cand_name}")
