2d72ba1be663d7b7ab98a4d2ec39d4b3fc8ccf00
Zach_Mason <zmason04@gmail.com>
2024-04-18 19:13:05 -0400

    pypoll working on percent votes of each cand

--- PyPoll/Analysis/main.py
+++ PyPoll/Analysis/main.py
@@ -7,8 +7,9 @@ csvpath = os.path.join('PyPoll','Resources','election_data.csv')
 #define any and all variables
 row_count = 0
 winner_votes = 0
-#cand_name = ''
+cand_name = ''
 candidate_list = []
+cand_votes = {}
 
 #open, read the csv file and then skip the header
 with open(csvpath) as csvfile:
@@ -19,16 +20,11 @@ with open(csvpath) as csvfile:
     #calculate the total number of votes by rows
     for row in csvreader:
         row_count +=1
-    print(f"Total votes: {row_count}")
+    print(f"Total Votes: {row_count}")
 
-        #module 3.3 lists/dictionaires
-      
-        #if/else statement to tally votes and add cand names
-        #if not already there
-    
+    #module 3.3 lists/dictionaires
     #hannah and dan help   
     #if cand_name in candidate_list:
-    
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     next(csvreader)
@@ -39,7 +35,23 @@ with open(csvpath) as csvfile:
         if cand_name not in candidate_list:
             candidate_list.append(cand_name)
         
+        if cand_name not in cand_votes:
+            cand_votes[cand_name] = 1
+        else:
+            cand_votes[cand_name] += 1
     print(f"Candidates List: ")
+    #delete this dupe section right below
+    #if cand_name not in candidate_votes:
+    #    candidate_votes[cand_name] = 1
+    #else:
+    #    candidate_votes[cand_name] += 1
 
     for cand_name in candidate_list:
         print(f"{cand_name}")
+
+    # Calculate and print the percentage of votes each candidate received
+    print("Percentage of votes per candidate:")
+    for cand_name in candidate_list:
+        votes = cand_votes[cand_name]
+        percent_votes = (votes / row_count) * 100
+        print(f"{cand_name} : {percent_votes:.3f}% ({votes})")
