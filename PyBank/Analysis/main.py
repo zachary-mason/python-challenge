7ce029e0562cb86e7b4261849f427c42b0f5baed
Zach_Mason <zmason04@gmail.com>
2024-04-18 16:22:24 -0400

    working script for PyBank before cleaning

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -6,8 +6,10 @@ csvpath = os.path.join('PyBank','Resources','budget_data.csv')
 
 #define variable
 total_net = 0
-great_inc = 0
-great_dec = 0
+great_inc_change = 0
+great_dec_change = 0
+great_inc_date = ''
+great_dec_date = ''
 row_count = 0
 prev_prof_loss = 0
 rows = []
@@ -16,9 +18,9 @@ monthly_change = []
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
 
-    #test to print out all rows in csv file
+    #test to see if able to read csv correctly
     #for rows in csvreader:
     #    print(rows)
     
@@ -34,20 +36,28 @@ with open(csvpath) as csvfile:
         if row_count > 1:
             month_change = int(row[1]) - (prev_prof_loss)
             monthly_change.append(month_change)
-        
-        prev_prof_loss = int(row[1])
 
+            #if statement to find max and min date and value
+            if month_change > great_inc_change:
+                great_inc_change = month_change
+                great_inc_date = row[0]
+                #great_inc_change = row[1]
+            elif month_change < great_dec_change:
+                great_dec_change = month_change
+                great_dec_date = row[0]
+                #great_inc_change = row[1]
+
+        prev_prof_loss = int(row[1])
+    
+    #testing the great inc/dec
+        #print(great_inc_change)
+        #print(great_inc_date)
+        #print(great_dec_change)
+        #print(great_dec_date)
+    
     #test print for total number of 
     # months/rows in data and total net
     #print(f"Total Months: {row_count}")
-    #print(f"Total: ${total_net}")
-
-#may delete this entire chunk
-#with open(csvpath) as csvfile:
-    #header = next(csvfile)
-    #total net $
-    #for row in csv.reader(csvfile):
-    #total_net += int(row[1])
     #print(f"Total: ${total_net}")
 
     #for row in csv.reader(csvfile):
@@ -62,6 +72,20 @@ avg_monthly_change = sum(monthly_change) / len(monthly_change)
 rounded_avg_monthly_change = round(avg_monthly_change, 2)
 #print(rounded_avg_monthly_change)            
 
-print(f"Total Months: {row_count}")
-print(f"Total: ${total_net}")
-print(f"Average Change: ${rounded_avg_monthly_change}")
+
+#module 3.2 slides/activities - first attempt
+output_file = os.path.join('PyBank','Analysis','results.csv')
+with open(output_file, 'w') as csvfile:
+    results_file = csv.writer(csvfile, delimiter=',')
+    results_file.writerow(["Financial Analysis"])
+    results_file.writerow(["----------------------------"])
+    results_file.writerow(["Total Months: " + f"{row_count}"])
+    results_file.writerow(["Total: " + f"${total_net}"])
+    results_file.writerow(["Average Change: " + f"${rounded_avg_monthly_change}"])
+    results_file.writerow(["Greatest Increase in Profits: " + f"{great_inc_date}" + " (" + f"${great_inc_change}" + ")"])
+    results_file.writerow(["Greatest Decrease in Profits: " + f"{great_dec_date}" + " (" + f"${great_dec_change}" + ")"])
+
+#printing out all to check results shown on the assignment
+#print(f"Total Months: {row_count}")
+#print(f"Total: ${total_net}")
+#print(f"Average Change: ${rounded_avg_monthly_change}")
