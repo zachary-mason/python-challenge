d085d1e0ef9da3d3a29b6fb040d315d1e5e2ebdf
Zach_Mason <zmason04@gmail.com>
2024-04-18 14:17:15 -0400

    updated script correctly printing through the average change

--- PyBank/Analysis/main.py
+++ PyBank/Analysis/main.py
@@ -4,30 +4,64 @@ import csv
 #create path
 csvpath = os.path.join('PyBank','Resources','budget_data.csv')
 
-#defin variable
+#define variable
 total_net = 0
 great_inc = 0
 great_dec = 0
 row_count = 0
+prev_prof_loss = 0
+rows = []
+monthly_change = []
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    
-   # print(csvreader)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    #for row in csvreader:
-       # print(row)
+    #test to print out all rows in csv file
+    #for rows in csvreader:
+    #    print(rows)
     
-    #total number of months
+    #had some help from Xpert learning assistance here
+    # and the avg change/monthly change
+    #counting the rows and total profit/loss 
+    #as it goes down the file
+    
     for row in csvreader:
         row_count +=1
-    print(f"Total Months: {row_count}")
+        total_net += int(row[1])
 
-with open(csvpath) as csvfile:
-    header = next(csvfile)
+        if row_count > 1:
+            month_change = int(row[1]) - (prev_prof_loss)
+            monthly_change.append(month_change)
+        
+        prev_prof_loss = int(row[1])
+
+    #test print for total number of 
+    # months/rows in data and total net
+    #print(f"Total Months: {row_count}")
+    #print(f"Total: ${total_net}")
+
+#may delete this entire chunk
+#with open(csvpath) as csvfile:
+    #header = next(csvfile)
     #total net $
-    for row in csv.reader(csvfile):
-        total_net +=int(row[1])
-    print(f"Total: ${total_net}")
+    #for row in csv.reader(csvfile):
+    #total_net += int(row[1])
+    #print(f"Total: ${total_net}")
+
+    #for row in csv.reader(csvfile):
+    #    if row_count > 1:
+    #       month_change = int(row[1]) - prev_prof_loss
+    #       monthly_change.append(monthly_change)
+    
+    #   prev_prof_loss = int(row[1])
+
+#average monthly change set up and round to 2 decimal points
+avg_monthly_change = sum(monthly_change) / len(monthly_change)
+rounded_avg_monthly_change = round(avg_monthly_change, 2)
+#print(rounded_avg_monthly_change)            
+
+print(f"Total Months: {row_count}")
+print(f"Total: ${total_net}")
+print(f"Average Change: ${rounded_avg_monthly_change}")
